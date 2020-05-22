9756c759abfa148c57af195ddc9f9d35187d0cf1
Andrei <9771919@mail.ru>
2020-05-22 11:20:21 +0300

    add styles for resise

--- src/сomponents/header/Header.js
+++ src/сomponents/header/Header.js
@@ -1,7 +1,6 @@
 import {ExcelComponent} from "../../core/ExcelComponent";
 
 export class Header extends ExcelComponent {
-
   static className = 'excel__header'
 
   toHTML() {
--- src/сomponents/table/table.template.js
+++ src/сomponents/table/table.template.js
@@ -1,23 +1,31 @@
-
 const CODES = {
   A: 65,
   Z: 90
 }
 
-function toSell() {
+function toСell() {
   return `
     <div class="cell" contenteditable></div>
   `
 }
 
 function toColumn(el) {
-  return `<div class="column">${el}</div>`
+  return `
+    <div class="column">  
+      ${el}
+      <div class="col-resize"></div>
+    </div>
+  `
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize"></div>' : ''
   return `
     <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+       </div>
       <div class="row-data">${content}</div>
     </div> 
   `
@@ -46,7 +54,7 @@ export function createTable(rowsCount = 15) {
   for(let i = 0; i < rowsCount; i++) {
     const cells = new Array(colsCount)
       .fill('')
-      .map(toSell)
+      .map(toСell)
       .join('')
     rows.push(createRow(i + 1, cells))
   }
