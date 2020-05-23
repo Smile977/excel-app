eeb150693706d485e11564a8b4fca7e990a3e6c7
Andrei <9771919@mail.ru>
2020-05-23 12:57:14 +0300

    Finish table resize

--- src/сomponents/table/Table.js
+++ src/сomponents/table/Table.js
@@ -1,5 +1,7 @@
 import {ExcelComponent} from "../../core/ExcelComponent";
 import {createTable} from "./table.template";
+import {resizeHandler} from "@/сomponents/table/table.resize";
+import {shouldResize} from "@/сomponents/table/table.functions";
 
 export class Table extends ExcelComponent{
   static className = 'excel__table'
@@ -15,12 +17,8 @@ export class Table extends ExcelComponent{
   }
 
   onMousedown(event) {
-    // console.log(event.target.getAttribute('data-resize'))
-    if (event.target.dataset.resize) {
-      console.log('Start resizing', event.target.dataset.resize)
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event)
     }
   }
-
-
-
 }
--- src/сomponents/table/table.template.js
+++ src/сomponents/table/table.template.js
@@ -3,16 +3,16 @@ const CODES = {
   Z: 90
 }
 
-function toСell() {
+function toСell(_, col) {
   return `
-    <div class="cell" contenteditable></div>
+    <div class="cell" contenteditable data-col="${col}"></div>
   `
 }
 
-function toColumn(el) {
+function toColumn(col, index) {
   return `
-    <div class="column">  
-      ${el}
+    <div class="column" data-type="resizable" data-col="${index}">  
+      ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
   `
@@ -21,7 +21,7 @@ function toColumn(el) {
 function createRow(index, content) {
   const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
-    <div class="row">
+    <div class="row" data-type="resizable">
       <div class="row-info">
         ${index ? index : ''}
         ${resize}
@@ -40,12 +40,11 @@ export function createTable(rowsCount = 15) {
   const colsCount = CODES.Z - CODES.A + 1
   const rows = []
 
-  // строка из букв A - Z
   const cols = new Array(colsCount)
-    .fill('') // заполнение ячеек пустой строкой
-    .map(toChar) // замена значения на букву от A до Z
-    .map(toColumn) //.map(el => toColumn(el) // передача элементов для колонок
-    .join('') // преобразование к строке
+    .fill('')
+    .map(toChar)
+    .map(toColumn)
+    .join('')
 
   rows.push(createRow(null, cols))
 
