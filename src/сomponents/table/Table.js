e1a45f740e2aa2e16462b1c4ad6cc656a3f5bdb0
Andrei <9771919@mail.ru>
2020-05-24 13:42:19 +0300

    add selected for table's cells

--- src/сomponents/table/Table.js
+++ src/сomponents/table/Table.js
@@ -1,24 +1,49 @@
-import {ExcelComponent} from "../../core/ExcelComponent";
-import {createTable} from "./table.template";
-import {resizeHandler} from "@/сomponents/table/table.resize";
-import {shouldResize} from "@/сomponents/table/table.functions";
+import {ExcelComponent} from "../../core/ExcelComponent"
+import {$} from "@core/dom"
+import {createTable} from "./table.template"
+import {resizeHandler} from "@/сomponents/table/table.resize"
+import {isCell, matrix, shouldResize} from "@/сomponents/table/table.functions"
+import {TableSelection} from "@/сomponents/table/TableSelection"
 
-export class Table extends ExcelComponent{
+export class Table extends ExcelComponent {
   static className = 'excel__table'
+
 
   constructor($root) {
     super($root, {
       listeners: ['mousedown']
-    });
+    })
+
   }
 
   toHTML() {
     return createTable(20)
   }
 
+  prepare() {
+    this.selection = new TableSelection()
+  }
+
+  init() {
+    super.init()
+
+    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selection.select($cell)
+  }
+
   onMousedown(event) {
-    if (shouldResize(event)) {
+    if (shouldResize(event)) { // resize cols and rows
       resizeHandler(this.$root, event)
+    } else if (isCell(event)) {
+      const $target = $(event.target)
+      if (event.shiftKey) { // select group cells
+        const $cells = matrix($target, this.selection.current)
+          .map(id => this.$root.find(`[data-id="${id}"]`))
+        this.selection.selectGroup($cells)
+      } else { // select one cell
+        this.selection.select($target)
+      }
     }
   }
 }
+
