537a078154bfeb2c65bb62cce1bc012b3858c5ce
Andrei <9771919@mail.ru>
2020-05-24 19:04:57 +0300

    add navigation cells

--- src/сomponents/table/Table.js
+++ src/сomponents/table/Table.js
@@ -2,18 +2,16 @@ import {ExcelComponent} from "../../core/ExcelComponent"
 import {$} from "@core/dom"
 import {createTable} from "./table.template"
 import {resizeHandler} from "@/сomponents/table/table.resize"
-import {isCell, matrix, shouldResize} from "@/сomponents/table/table.functions"
+import {isCell, matrix, nextSelector, shouldResize} from "@/сomponents/table/table.functions"
 import {TableSelection} from "@/сomponents/table/TableSelection"
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-
   constructor($root) {
     super($root, {
-      listeners: ['mousedown']
+      listeners: ['mousedown', 'keydown']
     })
-
   }
 
   toHTML() {
@@ -31,19 +29,55 @@ export class Table extends ExcelComponent {
     this.selection.select($cell)
   }
 
+  // onMousedown(event) {
+  //   if (shouldResize(event)) { // resize cols and rows
+  //     resizeHandler(this.$root, event)
+  //   } else if (isCell(event)) {
+  //     const $target = $(event.target)
+  //     if (event.shiftKey) { // select group cells
+  //       const $cells = matrix($target, this.selection.current)
+  //         .map(id => this.$root.find(`[data-id="${id}"]`))
+  //       this.selection.selectGroup($cells)
+  //     } else { // select one cell
+  //       this.selection.select($target)
+  //     }
+  //   }
+  // }
+
   onMousedown(event) {
-    if (shouldResize(event)) { // resize cols and rows
+    if (shouldResize(event)) {
       resizeHandler(this.$root, event)
     } else if (isCell(event)) {
       const $target = $(event.target)
-      if (event.shiftKey) { // select group cells
+      if (event.shiftKey) {
         const $cells = matrix($target, this.selection.current)
           .map(id => this.$root.find(`[data-id="${id}"]`))
         this.selection.selectGroup($cells)
-      } else { // select one cell
+      } else {
         this.selection.select($target)
       }
     }
   }
+
+  onKeydown(event) {
+    const keys = [
+      'Enter',
+      'Tab',
+      'ArrowLeft',
+      'ArrowRight',
+      'ArrowDown',
+      'ArrowUp'
+    ]
+
+    const {key} = event
+
+    if (keys.includes(key) && !event.shiftKey) {
+      event.preventDefault()
+      const id = this.selection.current.id(true)
+      const $next = this.$root.find(nextSelector(key, id))
+      this.selection.select($next)
+    }
+  }
 }
+
 
