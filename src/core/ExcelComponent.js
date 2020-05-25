69043f14df2087c0a885f785b5ab02850a060236
Andrei <9771919@mail.ru>
2020-05-25 12:41:25 +0300

    add Emitter for components

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -3,8 +3,9 @@ import {DomListener} from "./DomListener";
 export class ExcelComponent extends DomListener {
 
   constructor($root, options = {}) {
-    super($root, options.listeners);
+    super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
 
     this.prepare()
   }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,6 +13,11 @@ class Dom {
     return this.$el.outerHTML.trim()
   }
 
+  text(text) {
+
+    this.$el.textContent = text
+  }
+
   clear() {
     this.html('')
     return this
